dad0016915e81b0fe19d8e5eabe455a11577f5b6
Austin Rolert <73430185+IRRSSUE@users.noreply.github.com>
2021-07-22 23:40:21 +0630

    Update snakegame.py

--- snakegame.py
+++ snakegame.py
@@ -15,7 +15,7 @@ dis_width = 800
 dis_height = 600
 
 dis = pygame.display.set_mode((dis_width, dis_height))
-pygame.display.set_caption('Snake Game by Abhijitpatra.com')
+pygame.display.set_caption('Snake Game by IRRSSUE')
 
 clock = pygame.time.Clock()
 
